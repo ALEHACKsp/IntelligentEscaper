caa1a3a8782f5160e5a5d9d03b19180b4959eb17
lurumdare <8020186+lurumdare@users.noreply.github.com>
2019-10-04 08:31:27 +0300

    Добавил остальной функционал

--- IntelligentEscaper/MainForm.Designer.cs
+++ IntelligentEscaper/MainForm.Designer.cs
@@ -34,13 +34,29 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button17 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(16, 30);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(371, 336);
+            this.richTextBox1.Size = new System.Drawing.Size(321, 336);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -55,11 +71,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 372);
+            this.button1.Location = new System.Drawing.Point(97, 371);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.Size = new System.Drawing.Size(98, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Escape forced";
+            this.button1.Text = "Escape (forced)";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
@@ -82,19 +98,192 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(137, 371);
+            this.button2.Location = new System.Drawing.Point(16, 400);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(118, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Decode";
+            this.button2.Text = "Intelligent Unescape";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Escape";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(407, 371);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Escape URL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(500, 371);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Escape URL (Forced)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(633, 371);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Escape URL (Forced + Host)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(201, 371);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Intelligent Escape";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(407, 400);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(127, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Parse URL Parameters";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(540, 400);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(156, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Make URL with Parameters";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(16, 429);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Escape +";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(97, 429);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Escape #";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(407, 429);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "encodeURI";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(407, 457);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "decodeURI";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(500, 429);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(127, 23);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "encodeURIComponent";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(500, 457);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(127, 23);
+            this.button15.TabIndex = 16;
+            this.button15.Text = "decodeURIComponent";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(178, 429);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(79, 23);
+            this.button16.TabIndex = 17;
+            this.button16.Text = "Escape char:";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 431);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(343, 145);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(58, 75);
+            this.button17.TabIndex = 19;
+            this.button17.Text = "SWAP";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.Button17_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -116,6 +305,22 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button17;
     }
 }
 
--- IntelligentEscaper/MainForm.cs
+++ IntelligentEscaper/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using NiL.JS.BaseLibrary;
 using NiL.JS.Core;
@@ -15,36 +8,133 @@ namespace IntelligentEscaper
 {
     public partial class MainForm : Form
     {
+        private readonly Context context = new Context();
+        string swap = "";
+
         public MainForm()
         {
+            context.Eval(
+                "function ReverseString(str)\t//Helper Function\r\n{\r\n\tvar newstr = \"\";\r\n\t\r\n\tfor (var i = str.length - 1; i >= 0; i--)\r\n\t\tnewstr += str.charAt(i);\r\n\r\n\treturn newstr;\r\n}\r\n\r\nfunction LastOccurrenceOfChar(str, char)\t//Helper Function\r\n{\r\n\tvar found = ReverseString(str).indexOf(char);\r\n\t\r\n\tif (found != -1)\r\n\t\treturn str.length - 1 - found;\r\n\telse\r\n\t\treturn -1;\r\n}\r\n\r\nfunction CharToEncoded(char)\r\n{\r\n\tvar tmpstr = char.charCodeAt(0).toString(16).toUpperCase();\r\n\tvar newstr = \"\";\r\n\t\t\r\n\tif (tmpstr.length < 2)\r\n\t\tnewstr = \"%0\" + tmpstr;\r\n\telse if (tmpstr.length == 2)\r\n\t\tnewstr = \"%\" + tmpstr;\r\n\telse\r\n\t\tnewstr = \"%u\" + tmpstr;\r\n\r\n\treturn newstr;\r\n}\r\n\r\nfunction Escape(str, forced)\r\n{\r\n\tif (forced == null)\r\n\t\tforced = false;\r\n\t\r\n\tif (forced)\r\n\t\treturn ForcedEscape(str);\r\n\telse\r\n\t\treturn escape(str);\r\n}\r\n\r\nfunction ForcedEscape(str)\r\n{\r\n\tvar newstr = \"\";\r\n\tvar tmpstr;\r\n\r\n\tfor (var i = 0; i < str.length; i++)\r\n\t\tnewstr += CharToEncoded(str.charAt(i));\r\n\t\r\n\treturn newstr;\r\n}\r\n\r\nfunction ParseURLParameters(str)\r\n{\r\n\tvar anchortext = \"\";\r\n\tvar anchorfound = LastOccurrenceOfChar(str, \"#\");\r\n\t\r\n\tif (anchorfound != -1)\r\n\t{\r\n\t\tanchortext = str.substr(anchorfound);\r\n\t\tstr = str.substr(0, anchorfound);\r\n\t}\r\n\t\r\n\tvar paramsfound = str.indexOf(\"?\");\r\n\t\r\n\tif (paramsfound != -1)\r\n\t\tstr = str.substr(0, paramsfound) + \"\\n\\n\" + str.substr(paramsfound+1);\r\n\t\r\n\treturn unescape(str.replace(/&/g, \"\\n\").replace(/\\+/g, \" \")) + (anchortext==\"\"?\"\":\"\\n\\n\" + anchortext);\r\n}\r\n\r\nfunction UnescapeURLSpecialChars(str)\r\n{\r\n\tstr = str.replace(/%2F/g, \"/\");\r\n\t\r\n\treturn str;\r\n}\r\n\r\nfunction UnescapeHostSpecialChars(str)\r\n{\r\n\tstr = str.replace(/%2E/g, \".\");\r\n\t\r\n\tvar colon = str.indexOf(\"%3A\");\t\t//%3A = :\r\n\t\r\n\tif (colon != -1)\r\n\t\tstr = str.substr(0, colon) + unescape(str.substr(colon));\r\n\t\r\n\treturn str;\r\n}\r\n\r\nfunction EscapeURL(str, forced, forcehost)\r\n{\r\n\tif (forced == null)\r\n\t\tforced = false;\r\n\t\t\r\n\tif (forcehost == null)\r\n\t\tforcehost = false;\r\n\t\r\n\tvar anchortext = \"\";\r\n\tvar anchorfound = LastOccurrenceOfChar(str, \"#\");\r\n\t\r\n\tif (anchorfound != -1)\r\n\t{\r\n\t\tanchortext = str.substr(anchorfound);\r\n\t\tstr = str.substr(0, anchorfound);\r\n\t}\r\n\t\r\n\tvar newstr = \"\";\r\n\tvar processed = 0;\r\n\t\r\n\tvar protocol = str.indexOf(\"//\");\r\n\t\r\n\tif (protocol != -1)\r\n\t{\r\n\t\tnewstr += str.substr(processed, protocol + 2);\r\n\t\tprocessed += protocol + 2;\r\n\t}\r\n\t\r\n\tvar firstslash = str.substr(processed).indexOf(\"/\");\r\n\t\r\n\tif (firstslash != -1)\r\n\t{\r\n\t\tif (forcehost)\r\n\t\t\tnewstr += UnescapeHostSpecialChars(Escape(str.substr(processed, firstslash + 1), true));\r\n\t\telse\r\n\t\t\tnewstr += str.substr(processed, firstslash + 1);\r\n\r\n\t\tprocessed += firstslash + 1;\r\n\t}\r\n\t\r\n\tvar paramsbegin = str.substr(processed).indexOf(\"?\");\r\n\t\r\n\tif (paramsbegin != -1)\r\n\t{\r\n\t\tif (firstslash != -1)\r\n\t\t{\r\n\t\t\tnewstr += Escape(str.substr(processed, paramsbegin), forced);\r\n\t\t\tprocessed += paramsbegin;\r\n\t\t}\r\n\t\telse\r\n\t\t{\r\n\t\t\tif (forcehost)\r\n\t\t\t\tnewstr += UnescapeHostSpecialChars(Escape(str.substr(processed, paramsbegin), true));\r\n\t\t\telse\r\n\t\t\t\tnewstr += str.substr(processed, paramsbegin);\r\n\r\n\t\t\tprocessed += paramsbegin;\r\n\t\t}\r\n\t\t\r\n\t\tnewstr += \"?\";\r\n\t\t\r\n\t\tvar params_str = str.substr(processed + 1);\r\n\t\t\r\n\t\tvar params = params_str.split(\"&\");\r\n\t\t\r\n\t\tfor (var i = 0; i < params.length; i++)\r\n\t\t{\r\n\t\t\tvar param = params[i];\r\n\t\t\t\r\n\t\t\tif (i != 0)\r\n\t\t\t\tnewstr += \"&\";\r\n\t\t\t\r\n\t\t\tnewstr += EscapeParameter(param, forced);\r\n\t\t}\r\n\t}\r\n\telse\r\n\t\tif (firstslash != -1)\r\n\t\t\tnewstr += Escape(str.substr(processed), forced);\r\n\t\telse\r\n\t\t{\r\n\t\t\tif (forcehost)\r\n\t\t\t\tnewstr += UnescapeHostSpecialChars(Escape(str.substr(processed), true));\r\n\t\t\telse\r\n\t\t\t\tnewstr += str.substr(processed);\r\n\t\t}\r\n\t\r\n\tif (forced)\r\n\t\tnewstr = UnescapeURLSpecialChars(newstr);\r\n\t\r\n\treturn newstr + anchortext;\r\n}\r\n\r\nfunction EscapeParameter(str, forced)\r\n{\r\n\tif (forced == null)\r\n\t\tforced = false;\r\n\r\n\tvar equal = str.indexOf(\"=\");\r\n\t\r\n\tif (equal != -1)\r\n\t\treturn Escape(str.substr(0, equal), forced) + \"=\" + Escape(str.substr(equal+1), forced);\r\n\telse\r\n\t\treturn Escape(str, forced);\r\n}\r\n\r\nfunction MakeURLWithParameters(str)\r\n{\r\n\tvar newstr = \"\";\r\n\t\r\n\tvar lines = str.split(\"\\n\");\r\n\tvar params = 0;\r\n\t\r\n\tfor (var i = 0; i < lines.length; i++)\r\n\t\tif (i == 0)\r\n\t\t\tnewstr += EscapeURL(lines[i]);\r\n\t\telse\r\n\t\t{\r\n\t\t\tvar line = lines[i];\r\n\t\t\t\r\n\t\t\tif (line != \"\")\r\n\t\t\t{\r\n\t\t\t\tif (i == (lines.length-1) && line.charAt(0) == \"#\")\r\n\t\t\t\t\tnewstr += line;\r\n\t\t\t\telse\r\n\t\t\t\t{\r\n\t\t\t\t\tif (params == 0)\r\n\t\t\t\t\t\tnewstr += \"?\";\r\n\t\t\t\t\telse\r\n\t\t\t\t\t\tnewstr += \"&\";\r\n\t\t\t\t\t\r\n\t\t\t\t\tnewstr += EscapeParameter(line);\r\n\t\t\t\t\t\r\n\t\t\t\t\tparams++;\r\n\t\t\t\t}\r\n\t\t\t}\r\n\t\t}\r\n\t\r\n\treturn newstr;\r\n}\r\n\r\nfunction IntelligentUnescape(str)\r\n{\r\n\tvar laststr;\r\n\tvar newstr = str;\r\n\t\r\n\tdo\r\n\t{\r\n\t\tlaststr = newstr;\r\n\t\tnewstr = unescape(newstr);\r\n\t} while (laststr != newstr);\r\n\t\r\n\treturn newstr;\r\n}\r\n\r\nfunction IntelligentEscape(str)\r\n{\r\n\treturn escape(IntelligentUnescape(str));\r\n}\r\n\r\nfunction EscapeChar(str, char)\r\n{\r\n\tif (char.length > 0)\r\n\t{\r\n\t\tvar newstr = \"\";\r\n\t\t\r\n\t\tvar fchar = char.charAt(0);\r\n\t\tvar fcharenc = CharToEncoded(fchar);\r\n\t\t\r\n\t\tvar tmpchr;\r\n\t\t\r\n\t\tfor (var i = 0; i < str.length; i++)\r\n\t\t{\r\n\t\t\ttmpchr = str.charAt(i);\r\n\t\t\t\r\n\t\t\tif (tmpchr != fchar)\r\n\t\t\t\tnewstr += tmpchr;\r\n\t\t\telse\r\n\t\t\t\tnewstr += fcharenc;\r\n\t\t}\r\n\t\t\r\n\t\treturn newstr;\r\n\t}\r\n\telse\r\n\t\treturn str;\r\n}\r\n");
             InitializeComponent();
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            var context = new Context();
-
-            context.Eval("function CharToEncoded(char)\r\n{\r\n\tvar tmpstr = char.charCodeAt(0).toString(16).toUpperCase();\r\n\tvar newstr = \"\";\r\n\t\t\r\n\tif (tmpstr.length < 2)\r\n\t\tnewstr = \"%0\" + tmpstr;\r\n\telse if (tmpstr.length == 2)\r\n\t\tnewstr = \"%\" + tmpstr;\r\n\telse\r\n\t\tnewstr = \"%u\" + tmpstr;\r\n\r\n\treturn newstr;\r\n}  function ForcedEscape(str)\r\n{\r\n\tvar newstr = \"\";\r\n\tvar tmpstr;\r\n\r\n\tfor (var i = 0; i < str.length; i++)\r\n\t\tnewstr += CharToEncoded(str.charAt(i));\r\n\t\r\n\treturn newstr;\r\n}");
-
-            var concatFunction = context.GetVariable("ForcedEscape").As<Function>();
-
-            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("Escape").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, true}).ToString();
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            var context = new Context();
-
-            context.Eval("function IntelligentUnescape(str)\r\n{\r\n\tvar laststr;\r\n\tvar newstr = str;\r\n\t\r\n\tdo\r\n\t{\r\n\t\tlaststr = newstr;\r\n\t\tnewstr = unescape(newstr);\r\n\t} while (laststr != newstr);\r\n\t\r\n\treturn newstr;\r\n}");
-
+            swap = richTextBox1.Text;
             var concatFunction = context.GetVariable("IntelligentUnescape").As<Function>();
-
             richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
         }
 
-        private void Button3_Click(object sender, EventArgs e)
+        private void Button3_Click_1(object sender, EventArgs e)
         {
-            richTextBox1 = richTextBox2;
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("Escape").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeURL").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeURL").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, true}).ToString();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeURL").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, true, true}).ToString();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("IntelligentEscape").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("ParseURLParameters").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("MakeURLWithParameters").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeChar").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, "+"}).ToString();
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeChar").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, "#"}).ToString();
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("encodeURI").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("decodeURI").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("encodeURIComponent").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("decodeURIComponent").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text}).ToString();
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            swap = richTextBox1.Text;
+            var concatFunction = context.GetVariable("EscapeChar").As<Function>();
+            richTextBox2.Text = concatFunction.Call(new Arguments {richTextBox1.Text, textBox1.Text}).ToString();
+        }
+
+        private void Button17_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox2.Text;
+            richTextBox2.Text = swap;
+            swap = richTextBox1.Text;
         }
     }
 }
